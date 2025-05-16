3b217967e3582ac3ba1deec91110893947ca8706
Alioth <ngalioth@outlook.com>
2025-05-16 20:37:16 +0800

    feat: 一个repo拉倒了，整那些有的没的

---
+++ external/venti/readme.md
@@ -0,0 +1,5 @@
+# koishi-plugin-venti
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-venti?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-venti)
+
+just for personal usage
---
+++ external/venti/src/index.ts
@@ -0,0 +1,14 @@
+import { Context, Schema } from "koishi";
+
+export const name = "venti";
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({});
+
+export function apply(ctx: Context) {
+  // write your plugin here
+  ctx.command("gi").action((_, message) => "原神功能入口，目前还没实际功能等待完善");
+
+  ctx.command("hsr").action((_, message) => "铁道功能入口，目前还没实际功能等待完善");
+}
